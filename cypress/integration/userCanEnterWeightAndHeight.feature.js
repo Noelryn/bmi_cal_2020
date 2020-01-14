daefe1d7f69c9f15244db4e0b401e9b455875b17
Noel Ryan <noelgerard.ryan@gmail.com>
2020-01-14 17:34:33 +0100

    Adding test for button and adding button into index.html, no funtionality

--- cypress/integration/userCanEnterWeightAndHeight.feature.js
+++ cypress/integration/userCanEnterWeightAndHeight.feature.js
@@ -5,5 +5,6 @@ describe('user can enter weight and height', () => {
     cy.contains('BMI Calculator')
     cy.get('#weight').type('90')
     cy.get('#height').type('186')
+    cy.get('#Calculate').click()
 	})
 })
