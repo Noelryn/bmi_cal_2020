979137c17093453da7b4db202999c435a2eb1503
Noel Ryan <noelgerard.ryan@gmail.com>
2020-01-14 16:44:28 +0100

    scafholding the html in the index file and addding a test in the userCanEnterWeightAndHeight.feature.js file to find a H1 on the index.html

--- cypress/integration/userCanEnterWeightAndHeight.feature.js
+++ cypress/integration/userCanEnterWeightAndHeight.feature.js
@@ -1,6 +1,7 @@
 
 describe('user can enter weight and height', () => {
 	it('test', () => {
-		cy.visit('http://localhost:3001')
+    cy.visit('http://localhost:3001')
+    cy.contains('BMI Calculator')
 	})
 })
