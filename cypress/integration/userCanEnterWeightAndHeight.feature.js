e6ad04889d658074b673af685d0b5d1dcfa9bfcb
Noel Ryan <noelgerard.ryan@gmail.com>
2020-01-14 17:11:26 +0100

    Adding 2 tests for weight and height and adding the input fields for weight and height into the index.html

--- cypress/integration/userCanEnterWeightAndHeight.feature.js
+++ cypress/integration/userCanEnterWeightAndHeight.feature.js
@@ -3,5 +3,7 @@ describe('user can enter weight and height', () => {
 	it('test', () => {
     cy.visit('http://localhost:3001')
     cy.contains('BMI Calculator')
+    cy.get('#weight').type('90')
+    cy.get('#height').type('186')
 	})
 })
