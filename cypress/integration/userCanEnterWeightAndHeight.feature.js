f4eadd2c04e51261750ba52b2de1728b7c2de23b
Noel Ryan <noelgerard.ryan@gmail.com>
2020-01-15 15:34:45 +0100

    adding inline JavaScript to the button on the html file and creating a calculation funtion for the calculator

--- cypress/integration/userCanEnterWeightAndHeight.feature.js
+++ cypress/integration/userCanEnterWeightAndHeight.feature.js
@@ -1,6 +1,6 @@
 
-describe('user can enter weight and height', () => {
-	it('test', () => {
+describe('User can check their Bmi', () => {
+	it('user can enter weight and height', () => {
     cy.visit('http://localhost:3001')
     cy.contains('BMI Calculator')
     cy.get('#weight').type('90')
@@ -8,8 +8,7 @@ describe('user can enter weight and height', () => {
     cy.get('#Calculate').click()
   })
   
-  it('displays a BMI value of 26', () => {
-    cy.get('#results').should('contain', '26')
-  })
-  
+  it('displays a BMI value of 26.01', () => {
+    cy.get('#results').should('contain', '26.01')
+  }) 
 })
